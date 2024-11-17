5b1f42738e5269549067e0707efea79dabeb420f
Gilberto Froede <159205539+gilbertofroede@users.noreply.github.com>
2024-11-17 18:59:59 -0300

    Spamer Calçados

---
+++ PROJETO/script.js
@@ -0,0 +1,8 @@
+function mostrarInformacoes() {
+    var container = document.getElementById("informacoes-container");
+    if (container.style.display === "none") {
+        container.style.display = "block";
+    } else {
+        container.style.display = "none";
+    }
+}
